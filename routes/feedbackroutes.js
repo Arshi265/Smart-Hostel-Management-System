56ef3fbe35e0f132eaf358f72951ad6024da20d7
Arshi265 <arshi.bano265@gmail.com>
2026-04-12 12:16:32 +0530

    added feedback module with auth protection

---
+++ routes/feedbackroutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { 
+  createFeedback,
+  getAllFeedback,
+  deleteFeedback
+} from "../controllers/feedbackcontroller.js";
+
+import authMiddleware from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+
+//  CREATE FEEDBACK (LOGIN REQUIRED)
+router.post("/", authMiddleware, createFeedback);
+
+
+// GET ALL FEEDBACK (you can also protect it for admin later)
+router.get("/", getAllFeedback);
+
+
+//  DELETE FEEDBACK (optional protected)
+router.delete("/:id", authMiddleware, deleteFeedback);
+
+
+export default router;
