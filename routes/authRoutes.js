58403a8b7557ac8801321c9e98460e63b6e01f75
Arshi265 <arshi.bano265@gmail.com>
2026-04-13 18:29:57 +0530

    saved profile changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,10 @@ import { signup, login } from "../controllers/authController.js";
 import authMiddleware from "../middlewares/authmiddleware.js";
 // it connects frontend request to controller 
 const router = express.Router();
+// TEST ROUTE (IMPORTANT)
+router.get("/test", (req, res) => {
+  res.send("Auth routes working 🚀");
+});
 
 router.post("/signup", signup);
 router.post("/login", login);
@@ -14,4 +18,25 @@ router.get("/dashboard", authMiddleware, (req, res) => {
   });
 });
 
+
 export default router;
+// console.log("AUTH ROUTES LOADED ✅");
+// import express from "express";
+// import { signup, login } from "../controllers/authController.js";
+// import authMiddleware from "../middlewares/authmiddleware.js";
+
+// const router = express.Router();
+
+// // Public routes
+// router.post("/signup", signup);
+// router.post("/login", login);
+
+// // Protected route
+// router.get("/dashboard", authMiddleware, (req, res) => {
+//   res.json({
+//     message: "This is protected data",
+//     user: req.user,
+//   });
+// });
+
+// export default router;
