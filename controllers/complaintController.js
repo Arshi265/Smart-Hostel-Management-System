29346bfb745f8571ee3e7355010dcaaefe76d124
YourGitHubUsername <yadavmahesh9584@gmail.com>
2026-04-13 16:09:47 +0530

    complaitRoutes and rolemiddleware

--- controllers/complaintController.js
+++ controllers/complaintController.js
@@ -1,4 +1,4 @@
-import { Complaint } from "../models/complaint.js";
+import  Complaint  from "../models/complaints.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
